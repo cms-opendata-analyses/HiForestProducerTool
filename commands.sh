cc2ca2c0dd57c7fa298e0892068a13c2509f87cb
kati <k.lassila-perini@cern.ch>
2020-12-09 13:04:11 +0100

    Create 2010 branch, add commands

---
+++ commands.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+# parameters: $1 number of events (default: 100), $2 configuration file (default: hiforestanalyzer_cfg.py)
+sudo chown $USER /mnt/vol
+
+mkdir HiForest
+cd HiForest
+# For the plain github action with docker, the area would be available in /mnt/vol
+# git clone -b 2010 git://github.com/cms-legacy-analyses/HiForestProducerTool.git HiForestProducer
+cp -av /mnt/vol HiForestProducer
+cd HiForestProducer
+
+scram b -j8
+
+if [ -z "$1" ]; then nev=100; else nev=$1; fi
+if [ -z "$2" ]; then config=hiforestanalyzer_cfg.py; else config=$2; fi
+eventline=$(grep maxEvents $config)
+sed -i "s/$eventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" $config
+sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" $config
+cmsRun $config
+
+#root -l -b forest2dimuon.C
+
+cp *.root /mnt/vol/
+#cp *.png /mnt/vol/
+echo  ls -l /mnt/vol
+ls -l /mnt/vol

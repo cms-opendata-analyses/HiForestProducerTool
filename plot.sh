16cdb69548a5ea796fec1c8455dae80c6b23cb9b
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-12-12 23:09:13 +0200

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -5,8 +5,9 @@ mkdir HiForest
 cd HiForest
 # For the plain github action with docker, the area would be available in /mnt/vol
 # git clone -b 2010 git://github.com/cms-legacy-analyses/HiForestProducerTool.git HiForestProducer
-cp -av /mnt/vol/forest2dimuon.C HiForestProducer
+mkdir HiForestProducer
 cd HiForestProducer
+cp  /mnt/vol/forest2dimuon.C .
 
 cp /mnt/vol/*.root .
 root -l -b forest2dimuon.C

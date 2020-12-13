dd941c11f3400a7408edc0d75c28ff6f7ab338e3
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-12-13 18:22:59 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -6,15 +6,17 @@ mkdir HiForest
 cd HiForest
 # For the plain github action with docker, the repository is available in /mnt/vol
 # git clone -b 2011 git://github.com/cms-legacy-analyses/HiForestProducerTool.git HiForestProducer
-cp -av /mnt/vol HiForestProducer
+cp -r /mnt/vol HiForestProducer
 cd HiForestProducer
 
 scram b -j8
 
 if [ -z "$1" ]; then nev=100; else nev=$1; fi
 if [ -z "$2" ]; then config=hiforestanalyzer_cfg.py; else config=$2; fi
+# set the number of events
 eventline=$(grep maxEvents $config)
 sed -i "s/$eventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" $config
+# remove the connection to cvmfs, for GT access from docker container  
 sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" $config
 cmsRun $config
 
